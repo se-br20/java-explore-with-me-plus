24528517972593eadf7404fdcc67111fae64b3e7
Sergey <se.br2015@yandex.ru>
2026-07-13 22:35:01 +0300

    spring-cloud

--- infra/gateway-server/src/main/java/ru/practicum/infra/gateway/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/practicum/infra/gateway/GatewayServer.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(
-                GatewayServer.class,
-                args
-        );
+        SpringApplication.run(GatewayServer.class, args);
     }
 }
