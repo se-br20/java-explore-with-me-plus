8a6bc84034484a7f301169c76ed6a5442557e4b5
Sergey <se.br2015@yandex.ru>
2026-07-18 20:08:32 +0300

    feature/microservices

--- infra/gateway-server/src/main/java/ru/practicum/gateway/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/practicum/gateway/GatewayServer.java
@@ -2,13 +2,11 @@ package ru.practicum.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.practicum.gateway.startup.RequiredServicesStartupWaiter;
 
 @SpringBootApplication
 public class GatewayServer {
 
     public static void main(String[] args) {
-        RequiredServicesStartupWaiter.waitIfEnabled();
         SpringApplication.run(GatewayServer.class, args);
     }
 }
